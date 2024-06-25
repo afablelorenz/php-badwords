06b8de29ad8b01b727a12d33b98524a84f312ab9
afablelorenz <afablelorenz@gmail.com>
2024-06-25 17:41:01 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+    $paragrafo= "L'espressione è l'orecchio soffice di una produzione velenosa e qualche volta ci credi anche tu.";
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+
+
+        <script src="" async defer></script>
+    </body>
+</html>
